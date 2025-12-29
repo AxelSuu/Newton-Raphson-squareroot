4a52e74aa7a60fea26e265059579650dbdf41cdc
Axel Sundqvist <axesu672@student.liu.se>
2025-12-30 00:01:39 +0100

    Made changes so that the program could be run
    - added a main function
    - added some code which leetcode already used to run

--- solution.cc
+++ solution.cc
@@ -1,33 +1,42 @@
 // 69. sqrt(x) without built-in exponent function or operator, Newton-Raphson implementation
 // 0 ms runtime
+#include <cmath>
+#include <iostream>
 
-class Solution {
+class Solution
+{
 public:
     static constexpr int mySqrt(int x)
     {
-    if(x<=1)
-    {
-        return x;
-    }
-
-    double n = x;
-    double root{};
-    int count = 0;
-
-    while (true)
-    {
-        count++;
-
-        root = 0.5 * (n + (x / n));
-
-        if (abs(root - n) < 0.1)
+        if(x<=1)
         {
-            break;
+            return x;
         }
 
-        n = root;
-    }
+        double n = x;
+        double root{};
+        int count = 0;
 
-    return root;
+        while (true)
+        {
+            count++;
+
+            root = 0.5 * (n + (x / n));
+
+            if (std::abs(root - n) < 0.1)
+            {
+                break;
+            }
+
+            n = root;
+        }
+
+        return root;
     }
 };
+
+int main()
+{
+    Solution s{};
+    std::cout << s.mySqrt(16) << std::endl;
+}
